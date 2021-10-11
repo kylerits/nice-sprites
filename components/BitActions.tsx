b9e748426393272136d16d9887e95ed05c62a96b
Kyle Rodgers <kyle.rits.rodgers@gmail.com>
2021-10-11 16:15:49 -0400

    adjusting the look of the bit count buttons

--- components/BitActions.tsx
+++ components/BitActions.tsx
@@ -9,18 +9,20 @@ const BitActions: FC<Props> = ({bitCount, setBitCount}) => {
   return (
     <>
       <div className="bit-actions">
-        <button
-          className={bitCount == 16 ? "inline-flex items-center text-sm px-3 py-2 border-blue-500 lg:border-blue-600 border-2 rounded-md shadow-md text-white bg-blue-500 hover:bg-blue-600 duration-200 mb-4" : "inline-flex items-center text-sm px-3 py-2 border-blue-200 border-2 rounded-md shadow-md text-blue-600 hover:bg-gray-50 duration-200 mb-4"}
-          onClick={() => {setBitCount(16)}}
-        >
-          <span>16</span>
-        </button>
-        <button
-          className={bitCount == 32 ? "inline-flex items-center text-sm px-3 py-2 border-blue-500 lg:border-blue-600 border-2 rounded-md shadow-md text-white bg-blue-500 hover:bg-blue-600 duration-200 mb-4" : "inline-flex items-center text-sm px-3 py-2 border-blue-200 border-2 rounded-md shadow-md text-blue-600 hover:bg-gray-50 duration-200 mb-4"}
-          onClick={() => {setBitCount(32)}}
-        >
-          <span>32</span>
-        </button>
+        <span className="inline-flex flex-col rounded-md shadow-md border-2 border-blue-500">
+          <button
+            className={bitCount == 16 ? "inline-flex items-center text-sm px-3 py-2 text-white bg-blue-500 hover:bg-blue-600 duration-200" : "inline-flex items-center text-sm px-3 py-2 text-blue-400 hover:text-blue-600 duration-200"}
+            onClick={() => {setBitCount(16)}}
+          >
+            <span>16</span>
+          </button>
+          <button
+            className={bitCount == 32 ? "inline-flex items-center text-sm px-3 py-2 text-white bg-blue-500 hover:bg-blue-600 duration-200" : "inline-flex items-center text-sm px-3 py-2 text-blue-400 hover:text-blue-600 duration-200"}
+            onClick={() => {setBitCount(32)}}
+          >
+            <span>32</span>
+          </button>
+        </span>
       </div>
     </>
   );
